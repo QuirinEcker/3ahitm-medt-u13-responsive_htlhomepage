bf498270b43f0cfcd774d0626149eb3426f40632
Quirin <quirin.ecker@bajupa.com>
2019-12-08 13:34:37 +0100

    finished exercise

--- includes/nav.php
+++ includes/nav.php
@@ -2,25 +2,25 @@
 /* nav theme without images */
 ?>
 <nav class="row">
-	<div class="column">
+	<div class="column small">
 		<a class="it" href="informatik.php">
 			<h2>Informatik</h2>
 		</a>
 	</div>
 	
-	<div class="column">
+	<div class="column small">
 		<a class="medt" href="medientechnik.php">
 			<h2>Medientechnik</h2>
 		</a>
 	</div>
 	
-	<div class="column">
+	<div class="column small">
 		<a class="et" href="elektronik.php">
 			<h2>Elektronik</h2>
 		</a>
 	</div>
 	
-	<div class="column">
+	<div class="column small">
 		<a class="medz" href="medizintechnik.php">
 			<h2>Medizintechnik</h2>
 		</a>
